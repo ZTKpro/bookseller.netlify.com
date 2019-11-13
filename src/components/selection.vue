b4316a71f2a7317ecd67ca9f7f960954718a1cff
Damian <damianszmurlowork@gmail.com>
2019-11-13 14:54:42 +0100

    deleted usless code added new function search

--- src/components/selection.vue
+++ src/components/selection.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <h1>TEXT:{{this.book[1].url}}</h1>
-    <div  class="search_selection">
-      <div v-for="(item,value) in book" class="selection_bookbox">
-        <img class="bookbox_img" v-bind:src="item.url" alt="">
-        <p>{{item.tittle}}</p>
+    <div class="search_selection">
+      <div  v-bind:id="item.id" :key="item.id"  v-for="(item) in book" class="selection_bookbox">
+        <img v-on:click="checkBook" class="bookbox_img" v-bind:src="item.url" alt="">
+        <p v-on:click="checkBook" class="bookbox_tittle">{{item.tittle}}</p>
       </div>
     </div>
     <div class="search_customers">
@@ -73,11 +72,12 @@
           
           </ul>
         </div>
+
         <div
           class="box_custom"
         >
           <p>Klaudia Arcikiewicz</p>
-          <ul class="custom_ul">
+       
             <li
              
               class="custom_li"
@@ -99,8 +99,8 @@
               
               class="custom_li"
             >Świat fizyki Podręcznik. Zakres podstawowy</li>
-          </ul>
-          <p>9</p>
+
+        
         </div>
 
         <div
@@ -121,7 +121,7 @@
             >Spotkania z kulturą podręcznik Liceum i technikum</li>
             <li class="custom_li">PONAD SŁOWAMI 1.2 NOWA ERA</li>
           </ul>
-          <p>7</p>
+          
         </div>
 
         <div
@@ -143,7 +143,7 @@
               class="custom_li"
             >Fizyka - ODKRYĆ FIZYKĘ - Nowa Era - podstawowy</li>
           </ul>
-          <p>6</p>
+        
         </div>
 
 
@@ -172,7 +172,7 @@
               class="custom_li"
             >POZNAĆ PRZESZŁOŚĆ WIEK XX PODRĘCZNIK NOWA ERA</li>
           </ul>
-          <p>6</p>
+       
         </div>
 
         <div
@@ -197,7 +197,7 @@
               class="custom_li"
             >Świat fizyki Podręcznik. Zakres podstawowy</li>
           </ul>
-          <p>6</p>
+          
         </div>
 
         <div
@@ -222,7 +222,7 @@
             >Język polski Świat do przeczytania cz.1</li>
             <li class="custom_li">To jest chemia</li>
           </ul>
-          <p>6</p>
+          
         </div>
 
         <div
@@ -240,7 +240,7 @@
               class="custom_li"
             >POZNAĆ PRZESZŁOŚĆ WIEK XX PODRĘCZNIK NOWA ERA</li>
           </ul>
-          <p>5</p>
+         
         </div>
 
         <div
@@ -258,74 +258,101 @@
           </ul>
           <p>2</p>
         </div>
-
-        <!-- tutaj dodajesz nowych sprzedawców -->
-        <!-- tutaj dodajesz nowych sprzedawców -->
-        <!-- tutaj dodajesz nowych sprzedawców -->
       </div>
     </div>
-  </div>
 </template>
 <script>
 export default {
   data() {
   return {
+    // books
+    // books
+    // books
     book: [
-
-     {url:'https://a.allegroimg.com/s360/03381c/226a37c7453cb051fc1cb466397b',
+     {id:'kwk',
+     url:'https://a.allegroimg.com/s360/03381c/226a37c7453cb051fc1cb466397b',
      tittle:'Krok w przedsiębiorczość Podręcznik'},
         
-     {url:   'https://a.allegroimg.com/s360/03c64b/0e1ff22f4c8aadd1496f585c6c6f',
+     {id:'tj',url:   'https://a.allegroimg.com/s360/03c64b/0e1ff22f4c8aadd1496f585c6c6f',
      tittle:'To jest chemia Podręcznik + CD Zakres podstawowy' },
         
-     {url:  'https://e.allegroimg.com/s360/031e42/e57bbf3c418bb2253e3d4c02bf9e',
+     {id:'fof',url:  'https://e.allegroimg.com/s360/031e42/e57bbf3c418bb2253e3d4c02bf9e',
      tittle:' Fizyka - ODKRYĆ FIZYKĘ - Nowa Era - podstawowy' },
           
-     {url:   'https://4.allegroimg.com/s360/03b874/dd67a6254d81afcf3026e859b634',
+     {id:'og',url:   'https://4.allegroimg.com/s360/03b874/dd67a6254d81afcf3026e859b634',
      tittle:'Oblicza geografii'},
         
-     {url:  'https://7.allegroimg.com/s360/03fabd/d16f3ff247328422989b421c46f7'
+     {id:'bnc',url:  'https://7.allegroimg.com/s360/03fabd/d16f3ff247328422989b421c46f7'
     ,tittle:'Biologia na czasie'},
         
-     {url:'https://a.allegroimg.com/s360/03d23a/da5f8a8241919fde5b2b31a2f01d'
+     {id:'szk',url:'https://a.allegroimg.com/s360/03d23a/da5f8a8241919fde5b2b31a2f01d'
     ,tittle:'Spotkania z kulturą podręcznik Liceum i technikum'},
         
-     {url:  'https://c.allegroimg.com/s360/03385f/696aeb78425b9e9cf19d2dc0d96c '
+     {id:'wcu',url:  'https://c.allegroimg.com/s360/03385f/696aeb78425b9e9cf19d2dc0d96c '
     ,tittle:'W centrum uwagi'},
         
-     {url:  'https://6.allegroimg.com/s360/03057a/16b389f241f9b556416f607c8306 '
+     {id:'pns',url:  'https://6.allegroimg.com/s360/03057a/16b389f241f9b556416f607c8306 '
      ,tittle:'Ponad słowami podręcznik klasa 1 CZ. 1 NOWA ERA'},
         
-     {url:  'https://a.allegroimg.com/s360/0385bd/90d9dae842e38c2afde514766cfa '
+     {id:'ie',url:  'https://a.allegroimg.com/s360/0385bd/90d9dae842e38c2afde514766cfa '
      ,tittle:'Informatyka Europejczyka Podręcznik z płytą CD'},
         
-     {url:  'https://a.allegroimg.com/s360/03cb39/784c6d604149a34f395a3c14b1e9 '
+     {id:'sf',url:  'https://a.allegroimg.com/s360/03cb39/784c6d604149a34f395a3c14b1e9 '
      ,tittle:'Świat fizyki Podręcznik. Zakres podstawowy'},
 
-     {url: 'https://8.allegroimg.com/s360/031bf4/4b7fe93c447bb3b24a160be81cb8 '
+     {id:'wok',url: 'https://8.allegroimg.com/s360/031bf4/4b7fe93c447bb3b24a160be81cb8 '
     ,tittle:'Wiedza o kulturze SPOTKANIA Z KULTURĄ Podręcznik'},
 
-     {url:  'https://a.allegroimg.com/s360/034836/84528bff420dbaf631ddfaa60793 '
+     {id:'jps',url:  'https://a.allegroimg.com/s360/034836/84528bff420dbaf631ddfaa60793 '
     ,tittle:'Język polski Świat do przeczytania cz.1'},
         
-     {url:  'https://a.allegroimg.com/s360/03743d/bb43213045a7b7be819c312a2d12 '
+     {id:'pn',url:  'https://a.allegroimg.com/s360/03743d/bb43213045a7b7be819c312a2d12 '
      ,tittle:'PONAD SŁOWAMI 1.2 NOWA ERA'},
 
-    {url:  'https://a.allegroimg.com/s360/03a902/f42a3f3d497c9a36c9228ed3a2a7 '
+    {id:'m1',url:  'https://a.allegroimg.com/s360/03a902/f42a3f3d497c9a36c9228ed3a2a7 '
     ,tittle:'Matematyka 1. Zakres podstawowy i rozszerzony'},
         
-     {url:  'https://a.allegroimg.com/s360/038caf/3d01e4334207a0d4abab32f05576 '
+     {id:'iez',url: 'https://a.allegroimg.com/s360/038caf/3d01e4334207a0d4abab32f05576 '
      ,tittle:'Informatyka Europejczyka Zakres podstawowy Podr.'},
         
-     {url:  'https://e.allegroimg.com/s360/03adbb/a8f0d92142e585e3e9ce1449c41e '
+     {id:'mot' ,url:  'https://e.allegroimg.com/s360/03adbb/a8f0d92142e585e3e9ce1449c41e '
      ,tittle:'MATEMATYKA W OTACZAJĄCYM NAS ŚWIECIE 1 PODSTAWA'},
-     {url:  'https://a.allegroimg.com/s360/03ccae/e3f6bd3147b98247f8bddb3ab1bb '
+     {id:'cng',url:  'https://a.allegroimg.com/s360/03ccae/e3f6bd3147b98247f8bddb3ab1bb '
      ,tittle:'Czas na geografię Podręcznik Zakres podstawowy'},
         
-     {url:  'https://5.allegroimg.com/s360/03b1e2/3c474d744d908b543e20515bce25 '
+     {id:' pp',url:  'https://5.allegroimg.com/s360/03b1e2/3c474d744d908b543e20515bce25 '
      ,tittle:'Poznać przeszłość Wiek XX Podręcznik'},
+     ],
+
+
+
+    user:[
+      {name:'Klaudia Arcikiewicz', 
+      book:'',
+      }
      ]
-    }
+
+  }
+    },
+    methods: {
+      checkBook: function(e) {
+      e.target.parentElement.style.boxShadow = "0px 0px 35px blue";
+      const current = e.target.parentElement.textContent
+      console.log('current:  ' +current)
+      const li = [...document.querySelectorAll('.custom_li')]
+       let result = li;
+       result = result.filter(li => li.textContent.includes(current));
+       console.log(result)
+           for (let i = 0; i < result.length; i++) {
+        var c = result[i];
+        c.style.backgroundColor = "blue";
+       let parent = c.parentElement 
+       if(parent.parentElement.classList == 'box_custom'){
+         parent.parentElement.style.display = 'flex';
+       }
+        console.log( parent.parentElement.classList )
+      }
+    },
   },
 };
 </script>
@@ -342,13 +369,23 @@ export default {
 .selection_bookbox{
   height: 180px;
   width: 180px;
-  background-color: rgb(221, 221, 221);
-  margin-top: 10px;
+  background-color: rgb(199, 199, 199);
+  margin-top: 25px;
+  margin-bottom: 25px;
+  cursor: pointer;
 }
 .bookbox_img{
-  width: 100%;
+  width: auto;
   padding: 0px 40px;
-  height:auto;
+  height:120px;
+}
+.bookbox_tittle{
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  width: 180px;
+  background-color: white;
 }
 /* customers */
 /* customers */
@@ -392,7 +429,7 @@ export default {
 .custom_li {
   list-style: none;
   margin: 5px;
-  border: 1px solid rgb(221, 221, 221);
+  border: 1px solid rgb(180, 119, 119);
   padding: 5px;
 }
 
@@ -410,41 +447,4 @@ export default {
   border: none;
 }
 
-/* change this */
-/* change this */
-/* change this */
-
-.box_customactive0 {
-  display: flex;
-}
-.box_customactive1 {
-  display: flex;
-}
-.box_customactive2 {
-  display: flex;
-}
-.box_customactive3 {
-  display: flex;
-}
-.box_customactive4 {
-  display: flex;
-}
-.box_customactive5 {
-  display: flex;
-}
-.box_customactive6 {
-  display: flex;
-}
-.box_customactive7 {
-  display: flex;
-}
-.box_customactive8 {
-  display: flex;
-}
-.box_customactive9 {
-  display: flex;
-}
-.box_customactive10 {
-  display: flex;
-}
 </style>
